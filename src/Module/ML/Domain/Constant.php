539c7c5aa9f59e213ac65bb742b2a6246a67af3a
Rafał Mikołajun <root@rmweb.pl>
2024-05-25 13:48:16 +0200

    Added Constant class

---
+++ src/Module/ML/Domain/Constant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Module\ML\Domain;
+
+final class Constant
+{
+    public const string DEFAULT_SPAM_DATASET_FILENAME = 'spam_NLP.csv';
+    public const string DEFAULT_SPAM_CLEANSED_DATASET_FILENAME = 'spam_cleansed_NLP.csv';
+    public const int DEFAULT_TO_LONG_WORD_SIZE = 46;
+
+    public const array STOP_WORDS = [
+        'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves',
+        'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their',
+        'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+        'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+        'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about',
+        'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up',
+        'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when',
+        'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no',
+        'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don',
+        'should', 'now',
+    ];
+}
